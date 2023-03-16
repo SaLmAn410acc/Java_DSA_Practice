5144a179556ac33b697b8a593fa50509fce1699c
Salman <muhammadsalman410@gmail.com>
2023-03-16 16:08:21 +0500

    all comit

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class BubbleSort {
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    static void bubbleSort(int[] nums){
+
+        for (int i = nums.length-1 ; i >= 0; i--) {
+            boolean swap = false;
+            for (int j = 1; j <= i; j++) {
+                if (nums[j-1] > nums[j]) {
+                    int temp = nums[j-1];
+                    nums[j-1] = nums[j];
+                    nums[j] = temp;
+                    swap = true;
+                }
+            }
+
+            if(swap == false){
+                break;
+            }
+        }
+
+        System.out.println(Arrays.toString(nums));
+    }
 }
